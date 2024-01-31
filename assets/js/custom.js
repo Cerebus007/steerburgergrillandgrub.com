a5248d96fc85bfdaef852b02defe75c712ebebd3
Cody Beeston <cebeeston@gmail.com>
2024-01-31 13:07:36 -0600

    initial content push

---
+++ assets/js/custom.js
@@ -0,0 +1,29 @@
+/**
+ * Custom Scripts.
+ * Note: use this file to add or override any other scripts.
+ * DON'T EDIT TEMPLATE JS FILES DIRECTLY, JUST USE THIS FILE.
+ */
+
+;
+(function($, window, document, undefined) {
+
+    'use strict';
+
+    $(document).ready(function() {
+
+        /**
+         * Start Add Your New Scripts Below.
+         */
+
+
+    });
+
+})(jQuery, window, document);
+
+
+$('.hamburger').on('click', function () {
+
+	$('.hamburger-menu').toggleClass('expanded');
+
+});
+
